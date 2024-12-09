78907f2b07c6f7cacb4783ebf7573e91fd484424
adriannarendra <adrianenarendra@example.com>
2024-12-09 09:39:20 +0700

    lastlahyan

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -100,7 +100,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0eb75cb21f0a6c971d88ad52298c82a3
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "6bc498da885db4551d6b0ab71f4e3edb",
+"flutter_bootstrap.js": "2d3149520248ea102e6ed16302483c1b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
